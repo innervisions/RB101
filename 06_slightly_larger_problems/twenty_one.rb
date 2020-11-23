d9b067ad352a47397c9a5e64e3e7a9e6fc5b0e16
Raymond Saade <raymond.saade@gmail.com>
2020-11-22 22:52:02 -0500

    first draft twenty one

---
+++ 06_slightly_larger_problems/twenty_one.rb
@@ -0,0 +1,112 @@
+SUITS = %w(S C H D)
+RANKS = %w(Ace 2 3 4 5 6 7 8 9 10 Jack Queen King)
+
+def clear_screen
+  system('clear') || system('clr')
+end
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def initialize_deck
+  SUITS.each_with_object([]) do |suit, deck|
+    RANKS.each do |rank|
+      deck << { suit: suit, rank: rank }
+    end
+  end.shuffle
+end
+
+def joinand(arr)
+  case arr.size
+  when 1 then arr[0].to_s
+  when 2 then "#{arr[0]} and #{arr[1]}"
+  else
+    arr[0..-2].join(', ') + ", and #{arr[-1]}"
+  end
+end
+
+def ranks(cards) # takes array of cards, returns array of ranks
+  cards.map { |card| card[:rank] }
+end
+
+def display_hands(player_hand, dealer_hand, hole_hidden = false)
+  clear_screen
+  dealer_ranks = ranks(dealer_hand)
+  player_ranks = ranks(player_hand)
+  if hole_hidden
+    puts "Dealer has #{dealer_ranks[0]} and an unknown card."
+  else
+    puts "Dealer has #{joinand(dealer_ranks)}."
+  end
+  puts "You have #{joinand(player_ranks)}."
+  puts '-' * 40
+end
+
+def player_choice
+  loop do
+    prompt 'Would you like to hit or stay?:'
+    choice = gets.chomp.downcase
+    return 'hit' if 'hit'.start_with?(choice)
+    return 'stay' if 'stay'.start_with?(choice)
+    prompt "That's not a valid choice."
+  end
+end
+
+def total(hand)
+  hand_ranks = ranks(hand)
+  sum = 0
+  hand_ranks.each do |rank|
+    sum += if rank == 'Ace'
+             11
+           elsif rank.to_i == 0
+             10
+           else
+             rank.to_i
+           end
+  end
+  hand_ranks.count('Ace').times { sum -= 10 if sum > 21 }
+  sum
+end
+
+def display_results(player_hand, dealer_hand)
+  prompt "Dealer's hand is worth #{total(dealer_hand)}."
+  prompt "Your hand is worth #{total(player_hand)}."
+  prompt case total(player_hand) <=> total(dealer_hand)
+         when 1 then "You win!"
+         when -1 then "Dealer wins."
+         else "It's a push."
+         end
+end
+# MAIN #####
+deck = initialize_deck
+player_hand = [deck.pop, deck.pop]
+dealer_hand = [deck.pop, deck.pop]
+bust = false
+loop do
+  display_hands(player_hand, dealer_hand, true)
+  choice = player_choice
+  break if choice == 'stay'
+  player_hand << deck.pop
+  if total(player_hand) > 21
+    bust = true
+    break
+  end
+end
+
+if bust
+  display_hands(player_hand, dealer_hand, true)
+  prompt "You bust! Dealer wins."
+else
+  loop do
+    display_hands(player_hand, dealer_hand)
+    break if total(dealer_hand) >= 17
+    dealer_hand << deck.pop
+    bust = true if total(dealer_hand) > 21
+  end
+  if bust
+    prompt 'Dealer busts! You win!'
+  else
+    display_results(player_hand, dealer_hand)
+  end
+end
